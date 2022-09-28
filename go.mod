b0d09c018a85b7c97773d046fd972698aa797145
googs1025 <googs1025@gmail.com>
2022-09-28 22:12:17 +0800

    feat: 增加golang监听配置文件并支持热更新

--- go.mod
+++ go.mod
@@ -5,14 +5,19 @@ go 1.18
 require (
 	github.com/google/uuid v1.1.2
 	github.com/gorilla/mux v1.8.0
+	github.com/jpillora/overseer v1.1.6
 	go.etcd.io/etcd/client/v3 v3.5.5
+	gopkg.in/ini.v1 v1.67.0
 )
 
 require (
+	github.com/StackExchange/wmi v0.0.0-20190523213315-cbe66965904d // indirect
 	github.com/coreos/go-semver v0.3.0 // indirect
 	github.com/coreos/go-systemd/v22 v22.3.2 // indirect
+	github.com/go-ole/go-ole v1.2.4 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/jpillora/s3 v1.1.4 // indirect
 	go.etcd.io/etcd/api/v3 v3.5.5 // indirect
 	go.etcd.io/etcd/client/pkg/v3 v3.5.5 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
---
+++ myserver.go
@@ -0,0 +1,108 @@
+package main
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"flag"
+	"fmt"
+	"github.com/jpillora/overseer"
+	"gopkg.in/ini.v1"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"strconv"
+	"strings"
+	"syscall"
+	"time"
+)
+
+func main()  {
+	port:=flag.Int("p",0,"服务端口")
+	flag.Parse()
+	if *port==0{
+		log.Fatal("请指定端口")
+	}
+	cfg,err:=ini.Load("my.ini")
+	if err!=nil{
+		log.Fatal(err)
+	}
+	mux:=http.NewServeMux()
+	mux.HandleFunc("/",func(writer http.ResponseWriter, request *http.Request) {
+		dbUser:=cfg.Section("db").Key("db_user").Value()
+		dbPass:=cfg.Section("db").Key("db_pass").Value()
+		writer.Write([]byte("<h1>"+dbUser+"</h1>"))
+		writer.Write([]byte("<h1>"+dbPass+"</h1>"))
+	})
+	// 重新加载ini文件
+	mux.HandleFunc("/reload",func(writer http.ResponseWriter, request *http.Request) {
+		newCfg,_:=ini.Load("my.ini")
+		cfg=newCfg
+	})
+
+
+	server:=&http.Server{
+		Addr:":"+strconv.Itoa(*port),
+		Handler:mux,
+	}
+	prog:= func(state overseer.State) {
+		_ = server.Serve(state.Listener)
+	}
+
+	// 平滑重启
+	errChan:=make(chan error)
+	go func() {
+		overseer.Run(overseer.Config{
+			Program: prog,
+			TerminateTimeout:time.Second*2,
+			Address: ":"+strconv.Itoa(*port),
+		})
+	}()
+
+	//监听信号
+	go func() {
+		sigC :=make(chan os.Signal)
+		signal.Notify(sigC,syscall.SIGINT,syscall.SIGTERM)
+		errChan<-fmt.Errorf("%s",<-sigC)
+	}()
+
+	//监控配置文件变化
+	go func() {
+		fileMd5,err:=getFileMD5("my.ini")
+		if err!=nil{
+			log.Println(err)
+			return
+		}
+		for{
+			newMd5, err := getFileMD5("my.ini")
+			if err != nil {
+				log.Println(err)
+				break
+			}
+			if strings.Compare(newMd5,fileMd5) != 0 {
+				fileMd5=newMd5
+				fmt.Println("文件发生了变化")
+				// 如果文件发生变化，server执行重启操作
+				overseer.Restart()
+			}
+			time.Sleep(time.Second*2)
+		}
+	}()
+
+	getErr:=<-errChan
+
+	log.Println(getErr)
+}
+
+// 利用md5值判断文件是否修改
+func getFileMD5(filePath string) (string,error)  {
+	file, err := os.Open(filePath)
+	hash := md5.New()
+	if _, err = io.Copy(hash, file); err != nil {
+		return "",err
+	}
+	hashInBytes := hash.Sum(nil)[:16]
+	return hex.EncodeToString(hashInBytes),nil
+}
+
